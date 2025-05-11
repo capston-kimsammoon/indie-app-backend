bffcdd7bcb73d20449646f2e681bd87e648985fa
김가희 <kim536-9002@naver.com>
2025-05-11 23:22:36 +0900

    feat. DB 수정
    복합키 설정 및 불필요한 속성 삭제

--- app/models/user_artists_favorite.py
+++ app/models/user_artists_favorite.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, DateTime, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
 from app.database import Base
 import datetime
 
 class UserArtistFavorite(Base):
     __tablename__ = "user_artists_favorite"
+    
     user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
     artist_id = Column(Integer, ForeignKey("artists.id"), primary_key=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    __table_args__ = (
+        PrimaryKeyConstraint("user_id", "artist_id"),
+    )
