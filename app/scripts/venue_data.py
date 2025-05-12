4134d5865a0fa855273c7dbd3585c8f746d7626c
Kim hyunjin <hyun020227@naver.com>
2025-05-12 20:50:33 +0900

    venue 데이터 삽입

---
+++ app/scripts/venue_data.py
@@ -0,0 +1,584 @@
+venue_data = [
+    {
+        "name": '무대륙',
+        "address": '서울 마포구 토정로5길 12',
+        "region": '서울',
+        "instagram_account": 'mudaeruk'
+    },
+    {
+        "name": '살롱문보우',
+        "address": '서울 마포구 잔다리로 113 지층',
+        "region": '서울',
+        "instagram_account": 'salon.moonbow'
+    },
+    {
+        "name": '베이비돌 ',
+        "address": '서울 서대문구 연세로7안길 26 . 지하1층',
+        "region": '서울',
+        "instagram_account": 'babydoll_seoul'
+    },
+    {
+        "name": 'KT&G 상상마당 라이브홀',
+        "address": '서울 마포구 어울마당로 65 상상마당빌딩',
+        "region": '서울',
+        "instagram_account": 'ssmadang_live'
+    },
+    {
+        "name": '인천여관X루비살롱',
+        "address": '인천 중구 신포로35번길 17 1층',
+        "region": '인천',
+        "instagram_account": 'incheonmotel'
+    },
+    {
+        "name": '우주정거장(SpaceStation)',
+        "address": '서울 마포구 와우산로29길 16 지하 1층',
+        "region": '서울',
+        "instagram_account": 'spacestation2017'
+    },
+    {
+        "name": '스페이스 한강(Space Hangang)',
+        "address": '서울 마포구 와우산로 128 지하1층',
+        "region": '서울',
+        "instagram_account": 'space_hangang'
+    },
+    {
+        "name": '공상온도',
+        "address": '서울 마포구 동교로23길 40 지하',
+        "region": '서울',
+        "instagram_account": 'gongsangondo'
+    },
+    {
+        "name": '언플러그드 홍대점',
+        "address": '서울 마포구 와우산로33길 26 1층',
+        "region": '서울',
+        "instagram_account": 'unplugged_stage'
+    },
+    {
+        "name": 'BENDER LIVEHOUSE',
+        "address": '서울 마포구 와우산로14길 4 B1 (삼송빌딩)',
+        "region": '서울',
+        "instagram_account": 'bender_hongdae'
+    },
+    {
+        "name": '팡타개라지',
+        "address": '서울 종로구 경희궁1가길 7 복합문화공간에무 지하 1층 ',
+        "region": '서울',
+        "instagram_account": 'panta.emu'
+    },
+    {
+        "name": '클럽헤비',
+        "address": '대구 남구 현충로 244 지하',
+        "region": '대구',
+        "instagram_account": 'clubheavy'
+    },
+    {
+        "name": '프리즘홀',
+        "address": '서울 마포구 양화로12길 6 부광빌딩 B1',
+        "region": '서울',
+        "instagram_account": 'prismhall'
+    },
+    {
+        "name": '후케즈',
+        "address": '서울 영등포구 경인로 715 대영빌딩 지하 1층',
+        "region": '서울',
+        "instagram_account": 'hukez'
+    },
+    {
+        "name": '작은물',
+        "address": '서울 중구 을지로16길 6 3층',
+        "region": '서울',
+        "instagram_account": 'zak_eun_mul'
+    },
+    {
+        "name": '클럽온에어',
+        "address": '서울 마포구 잔다리로 44 1층 클럽온에어',
+        "region": '서울',
+        "instagram_account": 'club_onair'
+    },
+    {
+        "name": '클럽aor',
+        "address": '서울 마포구 와우산로 156 (서교동) 지하',
+        "region": '서울',
+        "instagram_account": 'clubaor'
+    },
+    {
+        "name": '무신사개러지',
+        "address": '서울 마포구 잔다리로 32 서문빌딩 B1 무신사 개러지',
+        "region": '서울',
+        "instagram_account": 'musinsagarage'
+    },
+    {
+        "name": 'CLUB FREEBIRD',
+        "address": '서울 서대문구 연희로 14 B1',
+        "region": '서울',
+        "instagram_account": 'clubfreebird'
+    },
+    {
+        "name": '라디오가가',
+        "address": '서울 마포구 양화로11길 54 지하1층',
+        "region": '서울',
+        "instagram_account": 'radl0_gaga'
+    },
+    {
+        "name": '바이널언더그라운드 소극장',
+        "address": '부산 남구 수영로322번길 32 지하',
+        "region": '부산',
+        "instagram_account": 'the_vinyl_underground'
+    },
+    {
+        "name": '롤링홀',
+        "address": '서울 마포구 어울마당로 35',
+        "region": '서울',
+        "instagram_account": 'rollinghall'
+    },
+    {
+        "name": 'CLUB VICTIM',
+        "address": '서울 마포구 와우산로 53 지하',
+        "region": '서울',
+        "instagram_account": 'club_victim'
+    },
+    {
+        "name": '모래내 극락',
+        "address": '서울 서대문구 수색로4길 7 2층 좌측호',
+        "region": '서울',
+        "instagram_account": 'morene_sukha'
+    },
+    {
+        "name": '클럽빵',
+        "address": '서울 마포구 와우산로29길 12 지하1층',
+        "region": '서울',
+        "instagram_account": 'clubbbang'
+    },
+    {
+        "name": '인터플레이',
+        "address": '대전 서구 대덕대로162번길 11 지하 인터플레이',
+        "region": '대전',
+        "instagram_account": 'interplay_livemusic'
+    },
+    {
+        "name": 'ACS',
+        "address": '서울 중구 수표로6길 10 지하1층',
+        "region": '서울',
+        "instagram_account": '_a_c_s___'
+    },
+    {
+        "name": '채널1969',
+        "address": '서울 마포구 연희로 35 지하 1층',
+        "region": '서울',
+        "instagram_account": 'channel1969.seoul'
+    },
+    {
+        "name": '제비다방',
+        "address": '서울 마포구 와우산로 24',
+        "region": '서울',
+        "instagram_account": 'jebidabang'
+    },
+    {
+        "name": '스트레인지프룻',
+        "address": '서울 마포구 와우산로29길 64',
+        "region": '서울',
+        "instagram_account": 'strangefruit.seoul'
+    },
+    {
+        "name": '클럽 에프에프',
+        "address": '서울 마포구 와우산로17길 12',
+        "region": '서울',
+        "instagram_account": 'hongdaeff'
+    },
+    {
+        "name": '오방가르드',
+        "address": '부산 남구 용소로7번길 15-1 지하 오방가르드',
+        "region": '부산',
+        "instagram_account": 'ovantgarde'
+    },
+    {
+        "name": '재미공작소',
+        "address": '서울 영등포구 선유로24길 10 2층',
+        "region": '서울',
+        "instagram_account": 'studio_zemi'
+    },
+    {
+        "name": '뮤직펍 펫사운즈',
+        "address": '서울 용산구 회나무로 21 3층',
+        "region": '서울',
+        "instagram_account": 'petsoundsmusicpub'
+    },
+    {
+        "name": '플렉스라운지(본점)',
+        "address": '서울 마포구 양화로 100-10 다내리빌딩',
+        "region": '서울',
+        "instagram_account": 'flex.lounge'
+    },
+    {
+        "name": '수상한창고',
+        "address": '서울 금천구 금하로24길 7 비1층',
+        "region": '서울',
+        "instagram_account": 'cpgdot'
+    },
+    {
+        "name": '청주 골목다방[복합문화공간]',
+        "address": '충북 청주시 상당구 사직대로361번길 8-14',
+        "region": '충북',
+        "instagram_account": 'live_cafe_golvang_kendama'
+    },
+    {
+        "name": '생기스튜디오',
+        "address": '서울 마포구 와우산로 137 5층 & Rooptop',
+        "region": '서울',
+        "instagram_account": 'senggistudio'
+    },
+    {
+        "name": '신도시',
+        "address": '서울 중구 수표동 11-2 5층',
+        "region": '서울',
+        "instagram_account": 'seendosi'
+    },
+    {
+        "name": '스팀펑크락',
+        "address": '서울 서대문구 연세로9길 13 지하1층',
+        "region": '서울',
+        "instagram_account": 'stimpunkrock_livepub'
+    },
+    {
+        "name": '나카스하이볼클럽',
+        "address": '서울 마포구 연희로1길 59 지층',
+        "region": '서울',
+        "instagram_account": 'nakasu_highball_club'
+    },
+    {
+        "name": '언드',
+        "address": '경남 거제시 거제대로 3734 지하1층 언드',
+        "region": '경남',
+        "instagram_account": 'und_space'
+    },
+    {
+        "name": '라이브홀 락왕',
+        "address": '대구 중구 명덕로 111',
+        "region": '대구',
+        "instagram_account": 'livehall_rock.king'
+    },
+    {
+        "name": '스튜디오산보',
+        "address": '서울 용산구 서빙고로79가길 32',
+        "region": '서울',
+        "instagram_account": 'studio.sanbo'
+    },
+    {
+        "name": '고요의 방',
+        "address": '서울 마포구 와우산로29길 14-10, 지층',
+        "region": '서울',
+        "instagram_account": 'goyoroom'
+    },
+    {
+        "name": '에무 복합문화공간',
+        "address": '서울 종로구 경희궁1가길 7',
+        "region": '서울',
+        "instagram_account": 'panta.emu'
+    },
+    {
+        "name": '클럽샤프',
+        "address": '서울 마포구 동교로 63 지하1층',
+        "region": '서울',
+        "instagram_account": 'club_sharp'
+    },
+    {
+        "name": '플레이그라운드1080',
+        "address": '제주 서귀포시 안덕면 신화역사로 195 플레이그라운드1080',
+        "region": '제주',
+        "instagram_account": 'playground_1080'
+    },
+    {
+        "name": 'Alleyway Taphouse',
+        "address": '경기 수원시 팔달구 정조로 751-11 중동빌딩',
+        "region": '경기',
+        "instagram_account": 'alleywaytaphouse'
+    },
+    {
+        "name": '소다2002',
+        "address": '서울 마포구 희우정로5길 29 1층',
+        "region": '서울',
+        "instagram_account": 'space_soda2002'
+    },
+    {
+        "name": '누구의 집',
+        "address": '인천 남동구 인주대로591번길 32 3층 301호',
+        "region": '인천',
+        "instagram_account": 'noogoo_jeeb'
+    },
+    {
+        "name": '석촌호수 아뜰리에',
+        "address": '서울 송파구 석촌호수로 191',
+        "region": '서울',
+        "instagram_account": 'art_space_sla'
+    },
+    {
+        "name": '인피니티클럽',
+        "address": '서울 서대문구 연세로7안길 34-3 지하1층',
+        "region": '서울',
+        "instagram_account": '1nfntclb'
+    },
+    {
+        "name": '언플러그드 라운지',
+        "address": '서울 마포구 와우산로29길 15 2층',
+        "region": '서울',
+        "instagram_account": 'unplugged_lounge'
+    },
+    {
+        "name": 'Booming Studios',
+        "address": '서울 강남구 도곡로 515 한도산업빌딩 지하 1층',
+        "region": '서울',
+        "instagram_account": 'theboomingstudios'
+    },
+    {
+        "name": '제임스레코드',
+        "address": '대구 중구 공평동 82-9',
+        "region": '대구',
+        "instagram_account": 'jamesrecord'
+    },
+    {
+        "name": 'Melodyne',
+        "address": '인천 남동구 논고개로 81 우리들타워 403호',
+        "region": '인천',
+        "instagram_account": 'cafe_melodyne'
+    },
+    {
+        "name": '엠피엠지',
+        "address": '서울 마포구 서강로 78',
+        "region": '서울',
+        "instagram_account": '78.avenue'
+    },
+    {
+        "name": '스페이스브릭',
+        "address": '서울 마포구 잔다리로 31 지하2층',
+        "region": '서울',
+        "instagram_account": 'spacebrickkorea'
+    },
+    {
+        "name": '꼬뮨',
+        "address": '대구 동성로5길 85 지하1층 ',
+        "region": '대구',
+        "instagram_account": 'communeslive'
+    },
+    {
+        "name": '합정 허밋',
+        "address": '서울 마포구 양화로6길 27 지하 1층',
+        "region": '서울',
+        "instagram_account": 'hermit_kr'
+    },
+    {
+        "name": '에릭스 펍',
+        "address": '경기 성남시 분당구 성남대로331번길 13',
+        "region": '경기',
+        "instagram_account": 'ericspubkorea'
+    },
+    {
+        "name": '부드러운 직선',
+        "address": '광주 북구 호동로 22-1 지하1층 (중흥동275-4)',
+        "region": '광주',
+        "instagram_account": 'club_boojik'
+    },
+    {
+        "name": '낮과밤',
+        "address": '제주 동광로21',
+        "region": '제주',
+        "instagram_account": 'club_day_and_night_jeju'
+    },
+    {
+        "name": '문화정원 아트홀',
+        "address": '서울 금천구 가산로 61 지하1층',
+        "region": '서울',
+        "instagram_account": 'cgarthall'
+    },
+    {
+        "name": '육일봉',
+        "address": '서울 중구 을지로16길 20 6층',
+        "region": '서울',
+        "instagram_account": 'youkillbong'
+    },
+    {
+        "name": '클럽 라이브앤라우드',
+        "address": '서울 마포구 동교로 191 디비엠빌딩 지하2층',
+        "region": '서울',
+        "instagram_account": 'liveandloud_music_company'
+    },
+    {
+        "name": '인디제주',
+        "address": '제주 제주시 광양11길 10 2층',
+        "region": '제주',
+        "instagram_account": 'indie_jeju_zero'
+    },
+    {
+        "name": '얼라이브홀',
+        "address": '서울 마포구 독막로7길 20 지층',
+        "region": '서울',
+        "instagram_account": 'alivehall'
+    },
+    {
+        "name": '클럽 일마레',
+        "address": '서울 마포구 포은로 75 2층',
+        "region": '서울',
+        "instagram_account": 'club_ilmare'
+    },
+    {
+        "name": 'cafe PPnF',
+        "address": '서울 종로구 성균관로 87 1F',
+        "region": '서울',
+        "instagram_account": 'fridaynightlive_kr'
+    },
+    {
+        "name": '서브라이엇 HBC',
+        "address": '서울 용산구 신흥로7길 1',
+        "region": '서울',
+        "instagram_account": 'subriot_hbc'
+    },
+    {
+        "name": '에프비커피 서교점',
+        "address": '서울 마포구 잔다리로 129 1층 에프비 커피로스터스',
+        "region": '서울',
+        "instagram_account": 'fbcoffee_roasters'
+    },
+    {
+        "name": '쉬바펍',
+        "address": '서울 서대문구 연세로 42-24 지층',
+        "region": '서울',
+        "instagram_account": 'shivapub'
+    },
+    {
+        "name": '부산 유기체',
+        "address": '부산 부산진구 서전로46번길 10-7 4층',
+        "region": '부산',
+        "instagram_account": 'yugiche'
+    },
+    {
+        "name": '나발',
+        "address": '대구 중구 달구벌대로446길 23-7',
+        "region": '대구',
+        "instagram_account": 'nabal_acoustic'
+    },
+    {
+        "name": '톤 스튜디오',
+        "address": '서울 마포구 성미산로 64-5 톤스튜디오 빌딩',
+        "region": '서울',
+        "instagram_account": 'tonestudio_kr'
+    },
+    {
+        "name": '클럽 스틸페이스',
+        "address": '서울 마포구 잔다리로 10 5층',
+        "region": '서울',
+        "instagram_account": 'clubsteelface'
+    },
+    {
+        "name": '창원블레이홀',
+        "address": '경남 창원시 성산구 외동 반림로 130번길 45',
+        "region": '경남',
+        "instagram_account": 'bley_hall_'
+    },
+    {
+        "name": '클럽노크',
+        "address": '인천 미추홀구 숙골로95번길 25 청사프라자 4층 노크',
+        "region": '인천',
+        "instagram_account": 'club_knock1'
+    },
+    {
+        "name": '우주파',
+        "address": '제주 제주시 탑동로 81 우주파',
+        "region": '제주',
+        "instagram_account": 'uzupa_theuniversewave'
+    },
+    {
+        "name": '코멘터리 사운드',
+        "address": '서울 마포구 월드컵로23길 18 201호',
+        "region": '서울',
+        "instagram_account": 'commentary.sound'
+    },
+    {
+        "name": '001클럽',
+        "address": '서울 마포구 와우산로18길 20',
+        "region": '서울',
+        "instagram_account": '001_club'
+    },
+    {
+        "name": '공감',
+        "address": '인천 중구 내동 16-1',
+        "region": '인천',
+        "instagram_account": 'empathy_fusion'
+    },
+    {
+        "name": '안양 퍼플',
+        "address": '경기 안양시 동안구 평촌대로 221 602, 603호 Purple Hall 퍼플홀',
+        "region": '경기',
+        "instagram_account": 'purple_hall'
+    },
+    {
+        "name": '클럽 지피',
+        "address": '전북 전주시 완산구 홍산북로 84-20 5층',
+        "region": '전북',
+        "instagram_account": 'hardrockclub_gp\n'
+    },
+    {
+        "name": '에브리데이나잇',
+        "address": '서울 강북구 도봉로10가길 12 지하1층',
+        "region": '서울',
+        "instagram_account": 'ever_ydaynight'
+    },
+    {
+        "name": '사운드마인드',
+        "address": '서울 관악구 낙성대로 14 지하',
+        "region": '서울',
+        "instagram_account": 'soundmindseoul'
+    },
+    {
+        "name": '강릉스톤',
+        "address": '강원 강릉시 중앙시장2길 14 1층',
+        "region": '강원',
+        "instagram_account": 'gnstone22'
+    },
+    {
+        "name": '타락',
+        "address": '대구 중구 삼덕동1가 21-9',
+        "region": '대구',
+        "instagram_account": 'livepub_tarock'
+    },
+    {
+        "name": '버스커스 ',
+        "address": '서울 강북구 도봉로87길 25 . 지하1층전체',
+        "region": '서울',
+        "instagram_account": 'buskers_live'
+    },
+    {
+        "name": '갤러리문래',
+        "address": '서울 영등포구 도림로131길 3 1층',
+        "region": '서울',
+        "instagram_account": 'gallerymullae'
+    },
+    {
+        "name": '완숙',
+        "address": '서울 서대문구 증가로10길 36-55 2층',
+        "region": '서울',
+        "instagram_account": 'wanandsook'
+    },
+    {
+        "name": '이들스',
+        "address": '서울 종로구 삼일대로 428 중2층 이들스',
+        "region": '서울',
+        "instagram_account": 'edls_official'
+    },
+    {
+        "name": '리얼라이즈',
+        "address": '부산 수영구 감포로8번길 15 지하1층',
+        "region": '부산',
+        "instagram_account": 'clubrealize'
+    },
+    {
+        "name": '테리픽 잼',
+        "address": '서울 서초구 방배천로2안길 38 1층',
+        "region": '서울',
+        "instagram_account": 'terrific_jam'
+    },
+    {
+        "name": '요이땅삐삐',
+        "address": '제주 제주시 한경면 고산로 19',
+        "region": '제주',
+        "instagram_account": 'yoittangpippi'
+    },
+]
