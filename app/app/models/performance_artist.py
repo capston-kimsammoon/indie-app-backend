7f67c4705e633528e13f0f443fa91e3f11e0a23f
rkgmlzla <kim536-9002@naver.com>
2025-07-28 00:55:43 +0900

    feat. DB 수정 및 라우터 추가

--- app/app/models/performance_artist.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class PerformanceArtist(Base):
-    __tablename__ = "performance_artists"
-    performance_id = Column(Integer, ForeignKey("performances.id"), primary_key=True)
-    artist_id = Column(Integer, ForeignKey("artists.id"), primary_key=True)
